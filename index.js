75917ee9550e0254ba59efdb43b1004da6101e24
aleksfeniks-web <aleks.feniks@gmail.com>
2026-05-14 17:53:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg');
+const { pool } = require('./db/pool');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
