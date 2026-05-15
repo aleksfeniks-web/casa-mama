e989bd847c96d73b68b1636b7afe053af7d17545
aleksfeniks-web <aleks.feniks@gmail.com>
2026-05-14 17:53:32 -0700

    Create pools.js

---
+++ db/pools.js
@@ -0,0 +1,12 @@
+// db/pool.js
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = { pool };
